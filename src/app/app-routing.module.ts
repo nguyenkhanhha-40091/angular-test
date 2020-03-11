d594050472c8c24983fabde7d1bbc672853a0e51
nguyenkhanhha-40091 <nguyenkhanhha.40091@gmail.com>
2020-03-11 21:07:27 +0700

    initial code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from "./page/about/about.component"
+import { DashboardComponent } from "./page/dashboard/dashboard.component"
+import { NotFoundComponent } from "./page/not-found/not-found.component"
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'home', component: DashboardComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { MasterLayoutComponent } from './layout/master-layout/master-layout.component';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { AboutComponent } from './page/about/about.component';
+import { NotFoundComponent } from './page/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MasterLayoutComponent,
+    DashboardComponent,
+    AboutComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
